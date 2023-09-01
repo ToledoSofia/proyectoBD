8aa789ca61525f2875b590edac0d76667de75e92
toledosofia <alumno24.toledo.sofia@ipm.edu.ar>
2023-09-01 17:43:43 -0300

    1,3 y parte del 4 parte 3

--- benjamin/botones.js
+++ benjamin/botones.js
@@ -1,6 +1,8 @@
+    conectarBD();
+    
     function conectarBD(){
         $.ajax({
-            url : "http://localhost:8080/inicio",
+            url : "http://localhost:8000/inicio",
             type : 'GET'
         })
 
@@ -16,10 +18,82 @@
         })
         // agarrarDatos();
    }
+   function insertarDatos(){
+        var formulario = new FormData();
+        // console.log($('formulario').serialize());
+        var nombre = document.getElementById("nombre").value;
+        var duracion = document.getElementById("duracion").value;
+        var tipo = document.getElementById("tipo").value;
+        var autores = document.getElementById("autores").value;
+        var url = document.getElementById("url").value;
+        let data = {"nombre": nombre, "duracion": duracion,"tipo": tipo,"autores": autores,"url": url};
+        console.log(data);
+
+        $.ajax({
+            url:"http://localhost:8000/sonidos",
+            type: "POST",
+            contentType : 'application/json',
+            data: JSON.stringify(data)
+            // data: $('form').serialize(),
+        })
+
+        .done(function(data){
+            console.log("joyaaaaaa");
+        })
+
+        .fail(function(jqXHR, textStatus, errorThrown){
+            console.log("Error de insercion de datos");
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        })
+    }
+    
+    function eliminarSonido(){
+        var id = document.getElementById("num").value;
+        $.ajax({
+            url: "http://localhost:8000/eliminarDatos/" + id,
+            type: 'DELETE'
+        })
+        
+        .done(function(data){
+            console.log("joyaaaaaa");
+        })
+
+        .fail(function(jqXHR, textStatus, errorThrown){
+            console.log("Error de eliminacion de datos");
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        })
+    }
+
+    function modificarAudio(){
+        var id = document.getElementById("num").value;
+        $.ajax({
+            url: "http://localhost:8000/modificarDatos/" + id,
+            type: 'PATCH',
+            contentType: "application/json",
+            data: JSON.stringify(data)
+        })
+
+        .done(function(data){
+            console.log("joyaaaaaa");
+        })
+
+        .fail(function(jqXHR, textStatus, errorThrown){
+            console.log("Error de eliminacion de datos");
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        })
+    }
+
+
 
    function agarrarLinkConId(id){
     $.ajax({
-        url : "http://localhost:8080/sonidos/" + id,
+        url : "http://localhost:8000/sonidos/" + id,
         type : 'GET'
     })
     .done(function(data){
@@ -40,7 +114,7 @@
 
    function agarrarDatos(id){
         $.ajax({
-            url : "http://localhost:8080/sonidosDatos/" + id,
+            url : "http://localhost:8000/sonidosDatos/" + id,
             type : 'GET'
         })
         .done(function(data){
@@ -68,4 +142,36 @@
             console.log(textStatus);
             console.log(errorThrown);
         })
-   }
+    }
+        function agarrarDatosCompletos(){
+            $.ajax({
+                url : "http://localhost:8000/sonidosDatos",
+                type : 'GET'
+            })
+            .done(function(data){
+                //console.log(data);
+                let divVacio = document.getElementById("datosRecibidos");
+                let nombre, duracion, tipo, autores, id, url;
+                divVacio.innerHTML = "<div style='padding-left:5%; padding-top:2%;'><h3><u>SONIDO:</u></h3></div>";
+    
+                for(i = 0; i<data.length; i++ ){
+                    //console.log(i);
+                    nombre = data[i].nombre;
+                    duracion = data[i].duracion;
+                    tipo = data[i].tipo;
+                    autores = data[i].autores;
+                    id = data[i].id;
+                    url = data[i].url;
+                    divVacio.innerHTML = divVacio.innerHTML + "<br> <div style='padding-left: 5%;'><li><u>SONIDO "+id+"</u>:</li><li>nombre: "+nombre+"</li> <li>duracion: " 
+                    +duracion+"s</li><li>tipo: "+tipo+"</li><li>autores: "+autores+"</li><li>url: "+url+"</li> </div>";
+                }
+            })
+    
+            .fail(function(jqXHR, textStatus, errorThrown){
+                console.log("ERROR2");
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+            })
+        }
+   
--- untitled1/index.js
+++ untitled1/index.js
@@ -5,7 +5,7 @@ let mysql = require('mysql');
 let app = express();
 app.use(cors());
 app.use(express.json());
-app.listen(8080);
+app.listen(8000);
 
 let conexion;
 
@@ -18,6 +18,29 @@ app.get('/inicio', function (solicitud, respuesta) {
     conexion.connect();
     conexion.query("use botoncitos;");
     respuesta.send("¡Hola Mundo!");
+})
+/*app.post("/insertar", function(solicitud, respuesta){
+    let nombre = solicitud.body.nombre;
+    let consulta = "INSERT INTO botoncitos (nombre, duracion, tipo, autores, url) values (" + nombre + ");";
+    conexion.query(consulta, function(resultado){
+        respuesta.send(resultado);
+    })
+
+})*/
+
+app.post('/sonidos', function (req, res){
+    console.log(req.body);
+   var nombre1 = req.body["nombre"];
+    var duracion = req.body["duracion"];
+    var tipo = req.body["tipo"];
+    var autores = req.body["autores"];
+    var url = req.body["url"];
+
+    let query = "insert into Sonido (nombre, duracion, tipo, autores, url) values ('" + nombre1 + "', '" + duracion + "', '" + tipo + "', '" + autores +"', '" + url +"');"
+    conexion.query(query, function (err, result, filed) {
+        if (err) throw err
+        console.log(result);
+    })
 })
 
 app.get('/fin', function (solicitud, respuesta) {
@@ -42,6 +65,14 @@ app.get('/sonidos/:id', function (solicitud, respuesta) {
         respuesta.send(resultado);
     });
 })
+app.get('/sonidosDatos', function (solicitud, respuesta) {
+    let id = solicitud.params.id;
+    let consulta = "SELECT * from Sonido;" ;
+    conexion.query(consulta, function (err,resultado){
+        if (err) throw err;
+        respuesta.send(resultado);
+    });
+})
 
 app.get('/sonidosDatos/:id', function (solicitud, respuesta) {
     let id = solicitud.params.id;
@@ -59,5 +90,40 @@ app.get('/sonidos', function (solicitud, respuesta) {
         }
         respuesta.send(filas);
     });
+})
+app.post('/inicio', function (solicitud, respuesta) {
+    conexion = mysql.createConnection({
+        host : 'localhost',
+        user : 'alumno',
+        password : 'alumnoipm'
+    });
+    conexion.connect();
+    conexion.query("use botoncitos;");
+    respuesta.send("¡Hola Mundo!");
+})
+
+app.delete("/eliminarDatos/:id", function(solicitud, respuesta){
+    let id = solicitud.params.id;
+    let consulta = 'DELETE from Sonido where id = ' + id + ';';
+    conexion.query(consulta, function (err,resultado){
+        if (err) throw err;
+        respuesta.send(resultado);
+    });
+})
+
+app.put ("modificarDatos/:id", function(solicitud, respuesta){
+    let id = solicitud.params.id;
+    let url = solicitud.body.url;
+    let nombre = solicitud.body.nombre;
+    let duracion = solicitud.body.duracion;
+    let tipo = solicitud.body.tipo;
+    let autores = solicitud.body.autores;
+
+    let consulta = 'UPDATE Sonido SET url = ' + url + ', nombre = ' + nombre + ', duracion = ' + duracion + ', tipo = ' + tipo + ', autores = ' + autores + ' where id = ' + id + ';';
+    conexion.query(consulta, function (err,resultado){
+        if (err) throw err;
+        respuesta.send(resultado);
+    });
+
 })
 
